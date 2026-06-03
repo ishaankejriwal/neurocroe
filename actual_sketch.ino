42dbe3311bad9bf7420cc4d130428f8b32db3caa
Ishaan Kejriwal <140764140+ishaankejriwal@users.noreply.github.com>
2026-06-03 14:14:52 -0400

    Refactor ICM_20948 IMU example for clarity

--- actual_sketch.ino
+++ actual_sketch.ino
@@ -1,115 +1,233 @@
-#include <Wire.h>
-#include <WiFi.h>
-#include <esp_now.h>
-#include "ICM_20948.h"
+/****************************************************************
+ * Example1_Basics.ino
+ * ICM 20948 Arduino Library Demo
+ * Use the default configuration to stream 9-axis IMU data
+ * Owen Lyke @ SparkFun Electronics
+ * Original Creation Date: April 17 2019
+ *
+ * Please see License.md for the license information.
+ *
+ * Distributed as-is; no warranty is given.
+ ***************************************************************/
+#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 
-// ================= CONFIG =================
-#define UNIT_ID 3
-#define SAMPLE_HZ 50
-#define DT_US (1000000 / SAMPLE_HZ)
-// ==========================================
+//#define USE_SPI       // Uncomment this to use SPI
 
-// ---------- SparkFun IMU ----------
-ICM_20948_I2C imu;
+#define SERIAL_PORT Serial
 
-// ---------- ESP-NOW PACKET ----------
-typedef struct __attribute__((packed)) {
-  uint8_t unit_id;
-  uint32_t t_ms;
-  float ax, ay, az;
-  float gx, gy, gz;
-} IMUPacket;
+#define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
+#define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
 
-uint8_t broadcastAddr[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+#define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
+// The value of the last bit of the I2C address.
+// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define AD0_VAL 1
 
-// ---------- RECEIVE FROM UNITS 1 & 2 ----------
-void onReceive(const esp_now_recv_info *info,
-               const uint8_t *data,
-               int len) {
-  if (len != sizeof(IMUPacket)) return;
+#ifdef USE_SPI
+ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
+#else
+ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
+#endif
 
-  IMUPacket pkt;
-  memcpy(&pkt, data, sizeof(pkt));
+void setup()
+{
 
-  // Forward wireless data to Python
-  Serial.printf(
-    "%d,%lu,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f\n",
-    pkt.unit_id,
-    pkt.t_ms,
-    pkt.ax, pkt.ay, pkt.az,
-    pkt.gx, pkt.gy, pkt.gz
-  );
-}
+  SERIAL_PORT.begin(115200);
+  while (!SERIAL_PORT)
+  {
+  };
 
-// ---------- TIMING ----------
-unsigned long nextMicros = 0;
+#ifdef USE_SPI
+  SPI_PORT.begin();
+#else
+  WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+#endif
 
-void setup() {
-  Serial.begin(115200);
+  //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
 
-  // ---------- I2C ----------
-  Wire.begin(21, 22);
-  Wire.setClock(400000);
+  bool initialized = false;
+  while (!initialized)
+  {
 
-  // ---------- SparkFun IMU ----------
-  bool ok = false;
-  while (!ok) {
-    imu.begin(Wire, 1); // AD0 = 1
-    if (imu.status == ICM_20948_Stat_Ok) {
-      ok = true;
-    } else {
+#ifdef USE_SPI
+    myICM.begin(CS_PIN, SPI_PORT);
+#else
+    myICM.begin(WIRE_PORT, AD0_VAL);
+#endif
+
+    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
+      SERIAL_PORT.println("Trying again...");
       delay(500);
     }
+    else
+    {
+      initialized = true;
+    }
   }
-
-  // ---------- ESP-NOW ----------
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-
-  if (esp_now_init() != ESP_OK) {
-    while (1);
-  }
-
-  esp_now_peer_info_t peerInfo = {};
-  memcpy(peerInfo.peer_addr, broadcastAddr, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  esp_now_add_peer(&peerInfo);
-
-  esp_now_register_recv_cb(onReceive);
-
-  Serial.println("BASE_READY");
-  nextMicros = micros();
 }
 
-void loop() {
-  unsigned long now = micros();
-  if ((long)(now - nextMicros) < 0) return;
-  nextMicros += DT_US;
+void loop()
+{
 
-  if (!imu.dataReady()) return;
+  if (myICM.dataReady())
+  {
+    myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
+                             //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
+    printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+    delay(30);
+  }
+  else
+  {
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
+}
 
-  imu.getAGMT();
+// Below here are some helper functions to print the data nicely!
 
-  // Accel: mg → g (normalized-ish like other units)
-  float ax = imu.accX() / 1000.0f;
-  float ay = imu.accY() / 1000.0f;
-  float az = imu.accZ() / 1000.0f;
+void printPaddedInt16b(int16_t val)
+{
+  if (val > 0)
+  {
+    SERIAL_PORT.print(" ");
+    if (val < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  else
+  {
+    SERIAL_PORT.print("-");
+    if (abs(val) < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  SERIAL_PORT.print(abs(val));
+}
 
-  float an = sqrt(ax*ax + ay*ay + az*az);
-  if (an > 0) { ax/=an; ay/=an; az/=an; }
+void printRawAGMT(ICM_20948_AGMT_t agmt)
+{
+  SERIAL_PORT.print("RAW. Acc [ ");
+  printPaddedInt16b(agmt.acc.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.z);
+  SERIAL_PORT.print(" ], Gyr [ ");
+  printPaddedInt16b(agmt.gyr.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.z);
+  SERIAL_PORT.print(" ], Mag [ ");
+  printPaddedInt16b(agmt.mag.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.z);
+  SERIAL_PORT.print(" ], Tmp [ ");
+  printPaddedInt16b(agmt.tmp.val);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
 
-  // Gyro: DPS → rad/s
-  float gx = imu.gyrX() * DEG_TO_RAD;
-  float gy = imu.gyrY() * DEG_TO_RAD;
-  float gz = imu.gyrZ() * DEG_TO_RAD;
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+  float aval = abs(val);
+  if (val < 0)
+  {
+    SERIAL_PORT.print("-");
+  }
+  else
+  {
+    SERIAL_PORT.print(" ");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++)
+  {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1))
+    {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+    {
+      tenpow *= 10;
+    }
+    if (aval < tenpow)
+    {
+      SERIAL_PORT.print("0");
+    }
+    else
+    {
+      break;
+    }
+  }
+  if (val < 0)
+  {
+    SERIAL_PORT.print(-val, decimals);
+  }
+  else
+  {
+    SERIAL_PORT.print(val, decimals);
+  }
+}
 
-  // Send Unit 3 data to Python
-  Serial.printf(
-    "%d,%lu,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f\n",
-    UNIT_ID,
-    millis(),
-    ax, ay, az,
-    gx, gy, gz
-  );
+#ifdef USE_SPI
+void printScaledAGMT(ICM_20948_SPI *sensor)
+{
+#else
+void printScaledAGMT(ICM_20948_I2C *sensor)
+{
+#endif
+  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+  printFormattedFloat(sensor->accX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accZ(), 5, 2);
+  SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+  printFormattedFloat(sensor->gyrX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrZ(), 5, 2);
+  SERIAL_PORT.print(" ], Mag (uT) [ ");
+  printFormattedFloat(sensor->magX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magZ(), 5, 2);
+  SERIAL_PORT.print(" ], Tmp (C) [ ");
+  printFormattedFloat(sensor->temp(), 5, 2);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
 }
